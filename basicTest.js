d9104b6e1b6e33a1c9e15f04d06a63b94869061e
Christian Kirschberg <ckirschberg@gmail.com>
2024-09-09 11:25:46 +0200

    .

--- basicTest.js
+++ basicTest.js
@@ -1,4 +1,5 @@
 import Person from "./Person.js";
+import Book from "./Book.js";
 
 let firstname = 'Christian';
 const birthDate = new Date(1979, 1, 1)
@@ -91,4 +92,90 @@ document.getElementById("newTodoText").addEventListener("keydown", (event) => {
 document.getElementById("addTodo").addEventListener("click", function() {
   createTodo();
 })
+
+
+const numbers = [4, 7, 9, 1.2, 5, 7];
+let sum = 0;
+for (let x of numbers) {
+    sum = sum + x;
+    console.log("Hello from the loop");
+}
+
+console.log("The result is " + sum);
+
+const randomArray = ['Peter', 7, 'Marianne', true, 'Helle', 8]
+
+randomArray.forEach(x => {
+    console.log(x + ' is a ' + typeof x);
+    console.log(`${x} is a ${typeof x}`);
+})
+
+
+const books = [ { 
+    title: "The Last Kingdom", 
+    author: "Bernard Cornwell" 
+}, {
+    title: "Beside Still Waters", 
+    author: "Robert Sheckley" 
+}, {
+    title: "The Dream of a Ridiculous Man", 
+    author: "Fyodor Dostoevsky"
+}]
+
+let myVar = "Harry Potter";
+const books2 = [
+    new Book(myVar, "Bernard Cornwell"),
+    new Book("Beside Still Waters", "Robert Sheckley"),
+    new Book("The Dream of a Ridiculous Man", "Fyodor Dostoevsky"),
+    new Book("The Dream of a Ridiculous Man 2", "Fyodor Dostoevsky"),
+    new Book("The Dream of a Ridiculous Man 3", "Fyodor Dostoevsky")
+]
+
+let time = 8;
+let greeting = "";
+if (time < 10) {
+    greeting = "Good morning";
+} else if (time < 15) {
+    greeting = "Good early day";
+} else if (time < 20) {
+    greeting = "Good late day";
+} else {
+    greeting = "I have kids, I am already asleep";
+}
+console.log(greeting);
+
+const email = 'kirs@cphbusiness.dk';
+const firstName = 'Christian';
+const lastname = 'Kirschberg';
+
+if (email === 'kirs@cphbusiness.dk' ||
+    firstName === 'Christian2' && lastname === 'Kirschberg'
+) {
+    console.log('Welcome!');
+}
+
+const number = "4"
+if (number === 4) {
+    console.log(true)
+}
+
+const pi = 3
+if (pi >= 3) {
+    console.log("pi is greater than 3")
+}
+if (pi < 3) { 
+    console.log("pi is less than 3")
+}
+const age = 18;
+let alcohol = age >= 18;
+console.log(alcohol);
+
+myObj.myMethod(admin ? 'ACTIVE' : 'NOT ALLOWED')
+
+const largest = (number1, number2, number3, number4) => {
+    //if ()
+    return number1;
+}
+
+console.log(largest(1, 2, 3, 4));
 
